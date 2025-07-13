b6cbdaafa5850fa5b069606052b2586e039fd1c1
Alvaro Araya <alvaroaraya345@gmail.com>
2025-07-13 05:13:45 -0400

    pruebas

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# es un test
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
